d067bf3e7e0d72cd6c0c020dc5e7ace56f3d722a
SBNCodeBase <131188515+SBNCodeBase@users.noreply.github.com>
2023-08-25 02:03:10 +0530

    added home screen

---
+++ src/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./HomeScreen.css"
+
+export default function HomeScreen() {
+  return (
+    <div class='container' display='flex' justifyContent='center' alignContent='center'>
+      HomeScreen
+    </div>
+  )
+}
